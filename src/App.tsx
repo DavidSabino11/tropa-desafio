3bfbd755a0c81159cec11d655b86042925c02a43
David Sabino <david.sabino17@gmail.com>
2022-06-03 18:35:53 -0300

    Tela de login em html/css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/global.css'
+import './styles/pages/login.css'
+import logo from './image/logo.svg'
+import notebook from './image/notebook.svg'
+import bemVindo from './image/bemVindo.svg'
+import rectangleBlack from './image/RectangleBlack.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='page-login'>
+      <div className="content-wrapper">
+        <div className="text-wrapper">
+          <img src={logo} alt="Logo" />
+          
+          <img src={bemVindo} className="bemVindo" alt="bemVindo" />
+
+          <input type="text" placeholder=' Digite seu email'></input>
+          <input type="text" placeholder=' Digite sua senha'></input>
+
+          <button>Acessar</button>
+        </div>
+        <div className="img-wrapper">
+        <div className="not">
+            <img src={notebook} alt="not" />
+          </div>
+          <div className="rec">
+            <img src={rectangleBlack} alt="not" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
